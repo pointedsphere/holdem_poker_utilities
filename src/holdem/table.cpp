9804b5c8a6f2552d61c8789e0dd16b0476dcee5f
Scott Donaldson <scottdonaldson88@gmail.com>
2021-09-29 01:19:01 +0100

    Hold cards class
    Used instead of something in hand class to reduce memory needed for deep copies of known hold cards later.

--- src/holdem/table.cpp
+++ src/holdem/table.cpp
@@ -2,4 +2,84 @@
 #include <vector>
 
 #include "table.h"
+#include "hand.h"
+
+
+
+/*
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                      HOLD CLASS
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
+void hold::setHoldCards(std::vector<int> holdInF, std::vector<int> holdInS, int noKnown)
+{
+
+  /*
+    Set the hold card for the current class (player), and the number of hold cards known.
+  */
+
+  noHoldKnown = noKnown;
+  for (int i=0; i<noKnown; i++) {
+    holdFace.push_back(holdInF[i]);
+    holdSuit.push_back(holdInS[i]);
+  }
+  
+}
+
+
+
+
+
+/*
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                     TABLE CLASS
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
+/*
+  Iitialise values/players
+*/
+
+int table::setNoPlayers(int noP)
+{
+
+  /*
+    
+    Set the number of players and initialise the array of hands class objects.
+
+    Note: If the number of players has already been set then all this data will be destroyed
+          then re-initialised.
+
+    RETURNS
+    =======
+    Returns an integer error/status code
+        0  :: Success!!
+	-1 :: noP not in [2,12].
+
+   */
+  
+  // There must be at least 2, and less than 12 players
+  if (noP<2 || noP>12) return -1;
+
+  // If number of players has already been set then destroy all that data
+  if (noPlayersSet_==true) {
+    D_.setDeckFull(); // Reset the deck to 52 cards
+    P_.clear();
+  } else {
+    noPlayersSet_ = true;
+  }
+  
+  // First set the number of players variable and not it is now set
+  noPlayers_ = noP;
+
+  // Now initialise the player class vector and the known hold cards for each player
+  for (int i=0; i<noPlayers_; i++) {
+    P_.push_back(hand());
+    H_.push_back(hold());
+  }
+  
+  return 0;
+  
+}
 
--- src/holdem/table.h
+++ src/holdem/table.h
@@ -4,22 +4,75 @@
 
 #include <vector>
 
+#include "deck.h"
+#include "hand.h"
+
+
+
+
+
+class hold
+{
+
+  /*
+
+    The class which holds just two hold cards
+
+    Note: This could go into the hand class, but we will hard copy this to save setting
+          it each time we loop for a Monte Carlo simulation, so we want to keep minimal
+	  ammount of data in this class as possible.
+
+  */
+
+public:
+  // Let everything about the hold cards be public (which is a touch unusual for the hold)
+
+  // Constructor
+  hold() {
+    noHoldKnown = 0;
+  }
+
+  int noHoldKnown; // The number of hold cards known, either 0, 1 or 2
+
+  // Vectors contianing face and suit values of hold cards
+  std::vector<int> holdFace;
+  std::vector<int> holdSuit;
+
+  // Set the hold cards
+  void setHoldCards(std::vector<int> holdInF, std::vector<int> holdInS, int noKnown);
+  
+};
+
+
+
+
+
 class table
 {
 
 private:
 
-  int noPlayers;    // Number of players at table
-  int noPlayersSet; // Has the number of players been set
+  int noPlayers_;    // Number of players at table
+  int noPlayersSet_; // Has the number of players been set
+
+  deck D_; // We need a deck on the table
+
+  std::vector<hand> P_; // A vector of players hands
+
+  std::vector<hold> H_; // A vector of each players hold cards (known and unknown)
   
 public:
-
+  
   // Constructor
   table() {
 
-    noPlayersSet = false; // Initially no players set
+    noPlayersSet_ = false; // Initially no players set
+    D_.setDeckFull(); // Initially we have a full deck
     
   }
+
+  // Initialise players
+  int setNoPlayers(int noP);
   
 };
 
