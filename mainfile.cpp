ae3823491c55b2f23317587a8db2f67f76ad6367
Scott Donaldson <scottdonaldson88@gmail.com>
2021-10-03 14:37:48 +0100

    Removed tests from main

--- mainfile.cpp
+++ mainfile.cpp
@@ -1,28 +1,16 @@
 #include<iostream>
 
+#include "src/tools/constants.h"
 #include "src/holdem/hand.h"
+#include "src/holdem/deck.h"
+#include "src/holdem/table.h"
 
 int main() {
-
-  /*
-    Initial example for the hand class.
-  */
   
-  int stat;
-  hand H;
-  int hole[2][2] = {{3,4},{1,3}};
-  int flop[2][3] = {{14,9,13},{1,4,3}};
-  int turn[2][1] = {{8},{3}};
-  int river[2][1]= {{1},{2}};
-  
-  stat = H.setCards(hole,flop,turn,river);
-  std::cout << "status a: " << stat << std::endl;
-  stat = H.findBestHand();
-  std::cout << "status b: " << stat << std::endl;
-  std::cout << "hand code: " << H.handCode << std::endl;
-  std::vector<int> T = H.getCardsFace();
-  for(int i=0; i<5; i++) {
-    std::cout << H.bestFace[i] << "   " << T[i] << std::endl;
-  }
   return 0;
+  
 }
+
+
+
+
