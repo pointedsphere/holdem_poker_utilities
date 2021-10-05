a36ee7d309fea97b12649e5c89cba05c306a6e45
Scott Donaldson <scottdonaldson88@gmail.com>
2021-10-05 01:54:05 +0100

    Efficiency improvement
    Only check royal/straight flush if we have a flush to avoid the large switch block.

--- machine_gen_code/parseDataToFunction.py
+++ machine_gen_code/parseDataToFunction.py
@@ -5,9 +5,9 @@ outFile = "checkHandPrime.cpp"
 
 # Function header
 with open(outFile, "a") as f:
-    f.write("int primefinderFace(int AP1, int AP2, int AP3, int AP4, int AP5, int AP6, int AP7,\\\n")
-    f.write("                    int FP1, int FP2, int FP3, int FP4, int FP5, int FP6, int FP7,\\\n ")
-    f.write("                   int SP1, int SP2, int SP3, int SP4, int SP5, int SP6, int SP7) \n{\n\n")
+    f.write("int findHandPrime(int AP1, int AP2, int AP3, int AP4, int AP5, int AP6, int AP7,\\\n")
+    f.write("                  int FP1, int FP2, int FP3, int FP4, int FP5, int FP6, int FP7,\\\n ")
+    f.write("                 int SP1, int SP2, int SP3, int SP4, int SP5, int SP6, int SP7) \n{\n\n")
     f.write("  /*\n")
     f.write("    Return the integer Hand Code value from the input face and suit card values\n")
     f.write("    Prime values for all calrds {AP1,...,AP7} take a sequential form from 2 to ace\n")
@@ -49,30 +49,74 @@ with open(outFile, "a") as f:
     f.write("  */\n\n")
 
 
+
+    #
+    # Flush
+    #
+
+    f.write("  /*\n")
+    f.write("  // We first check for a flush as thsi requires a trivial amount of switch cases and will allow\n")
+    f.write("  // us to skip the check for straight and royal flush if we dont have any form of flush.\n")
+    f.write("  // this reuires the product of the prime suit values.\n")
+    f.write("  */\n\n")
+    
+    # Initial switch declaration
+    f.write("  // Boolean to note if we have a flush or not.\n")
+    f.write("  bool gotFlush;\n\n")
+    f.write("  // Calculate the product value required for the flush hands from suit primes,\n")
+    f.write("  // I.e. product of card values from the ``suit prime'' deck.\n")
+    f.write("  const long long int SuitProd = SP1 * SP2 * SP3 * SP4 * SP5 * SP6 * SP7;\n\n")
+    f.write("  // Then use a swich statement over all the possible full houses and 4 of a kinds.\n")
+    f.write("  // Note: If we find one the function exits in this switch.\n")
+    f.write("  // Note: Start with full house checks as they are more likely.\n")
+    f.write("  switch(SuitProd) {\n")
+
+    # Load the data from the data file for the suit only hands
+    SuitArr = []
+    with open("./gen_flush/flush.dat", "r") as fIn:
+        for l in fIn:
+            if not l.startswith("//"):
+                l = l.split(",")
+                SuitArr.append((int(l[0]),int(l[1])))
+                
+    SuitArr = sorted(SuitArr, key=lambda tup: tup[0])
+
+    # Write the switch cases first for full houses, all in this file are flushes
+    for i in range(len(SuitArr)):
+        f.write("  case ")
+        f.write(str(SuitArr[i][0]))
+        f.write(" : gotFlush = true;\n")
+
+    # Now close that switch 
+    f.write("  }\n\n\n\n")    
+
+
     
     #
     # Royal and straight flush checks
     #
 
     f.write("  /*\n")
-    f.write("  We first check for the royal and straight flush, as this allows us to dicard the best hands outright.\n\n")
+    f.write("    We now check for the royal and straight flush, as this allows us to dicard the best hands outright.\n\n")
 
-    f.write("  With our `primes' method we can only check for lower hands once we know there is no hand that\n")
-    f.write("  relies on both suit and face values of the cards.\n\n")
+    f.write("    With our `primes' method we can only check for lower hands once we know there is no hand that\n")
+    f.write("    relies on both suit and face values of the cards.\n\n")
 
-    f.write("  This does require using the full prime card values, not just the suit and face values so\n")
-    f.write("  we need 64 bit long long integers.\n")
+    f.write("    This does require using the full prime card values, not just the suit and face values so\n")
+    f.write("    we need 64 bit long long integers.\n")
+    f.write("    NOTE: we only check if there is some form of flush.\n")
     f.write("  */\n\n")
 
-    
+    f.write("  // Only check for a striaght/royal flush if we have a flush of some sort.\n")
+    f.write("  if (gotFlush==true) {\n")
     # Initial switch declaration
-    f.write("  // Calculate the product value required for the Royal and Straight Flush hands,\n")
-    f.write("  // I.e. product of card values from the ``all prime'' deck.\n")
-    f.write("  const long long int AllProd = AP1 * AP2 * AP3 * AP4 * AP5 * AP6 * AP7;\n\n")
-    f.write("  // Then use a swich statement over all the possible straight and royal flushes.\n")
-    f.write("  // Note: If we find one the function exits in this switch.\n")
-    f.write("  // Note: Start with straight flush checks as they are slightly more likely.\n")
-    f.write("  switch(AllProd) {\n")
+    f.write("    // Calculate the product value required for the Royal and Straight Flush hands,\n")
+    f.write("    // I.e. product of card values from the ``all prime'' deck.\n")
+    f.write("    const long long int AllProd = AP1 * AP2 * AP3 * AP4 * AP5 * AP6 * AP7;\n\n")
+    f.write("    // Then use a swich statement over all the possible straight and royal flushes.\n")
+    f.write("    // Note: If we find one the function exits in this switch.\n")
+    f.write("    // Note: Start with straight flush checks as they are slightly more likely.\n")
+    f.write("    switch(AllProd) {\n")
 
     # Load the data from the data file for the straight and royal flushes
     SRFarr = []
@@ -87,29 +131,29 @@ with open(outFile, "a") as f:
     # Print the switch cases first for the straight flushes
     for i in range(len(SRFarr)):
         if (SRFarr[i][1]==9):
-            f.write("  case ")
+            f.write("    case ")
             f.write(str(SRFarr[i][0]))
             f.write(" : return 9;\n")
 
     # And then for the royal flushes
     for i in range(len(SRFarr)):
         if (SRFarr[i][1]==10):
-            f.write("  case ")
+            f.write("    case ")
             f.write(str(SRFarr[i][0]))
             f.write(" : return 10;\n")
 
-    # Now close that switch 
-    f.write("  }")
-
+    # Now close that switch and if
+    f.write("    }\n\n")
+    f.write("  }\n")
 
     
     #
     # Four of a kind and full house
     #
 
-    f.write("  \n\n\n\n\n/*\n")
-    f.write("  We now check for four of a kind and full house, this requires the prime product from the\n")
-    f.write("  prime face values of the cards only.\n")
+    f.write("  \n\n\n\n\n  /*\n")
+    f.write("    We now check for four of a kind and full house, this requires the prime product from the\n")
+    f.write("    prime face values of the cards only.\n")
     f.write("  */\n\n")
     
     # Initial switch declaration
@@ -154,35 +198,21 @@ with open(outFile, "a") as f:
     # Flush
     #
     
-    f.write("  \n\n\n\n\n/*\n")
-    f.write("  We now check for a flush, which uses only the product of the prime suit values.\n")
+    f.write("  \n\n\n\n\n  /*\n")
+    f.write("    We now check for a flush, using boolean from previous check.\n")
     f.write("  */\n\n")
     
-    # Initial switch declaration
-    f.write("  // Calculate the product value required for the flush hands from suit primes,\n")
-    f.write("  // I.e. product of card values from the ``suit prime'' deck.\n")
-    f.write("  const long long int SuitProd = SP1 * SP2 * SP3 * SP4 * SP5 * SP6 * SP7;\n\n")
-    f.write("  // Then use a swich statement over all the possible full houses and 4 of a kinds.\n")
-    f.write("  // Note: If we find one the function exits in this switch.\n")
-    f.write("  // Note: Start with full house checks as they are more likely.\n")
-    f.write("  switch(SuitProd) {\n")
+    # # Initial switch declaration
+    # f.write("  // Calculate the product value required for the flush hands from suit primes,\n")
+    # f.write("  // I.e. product of card values from the ``suit prime'' deck.\n")
+    # f.write("  const long long int SuitProd = SP1 * SP2 * SP3 * SP4 * SP5 * SP6 * SP7;\n\n")
+    # f.write("  // Then use a swich statement over all the possible full houses and 4 of a kinds.\n")
+    # f.write("  // Note: If we find one the function exits in this switch.\n")
+    # f.write("  // Note: Start with full house checks as they are more likely.\n")
+    # f.write("  switch(SuitProd) {\n")
 
-    # Load the data from the data file for the suit only hands
-    SuitArr = []
-    with open("./gen_flush/flush.dat", "r") as fIn:
-        for l in fIn:
-            if not l.startswith("//"):
-                l = l.split(",")
-                SuitArr.append((int(l[0]),int(l[1])))
-                
-    SuitArr = sorted(SuitArr, key=lambda tup: tup[0])
-
-    # Write the switch cases first for full houses, all in this file are flushes
-    for i in range(len(SuitArr)):
-        f.write("  case ")
-        f.write(str(SuitArr[i][0]))
-        f.write(" : return 6;\n")
-
+    f.write("  if (gotFlush==true) {\n")
+    f.write("    return 6;\n")
     # Now close that switch 
     f.write("  }")
 
@@ -193,9 +223,9 @@ with open(outFile, "a") as f:
     # Straight, 3 of a kind, 2 pair, pair
     #
     
-    f.write("  \n\n\n\n\n/*\n")
-    f.write("  We now check for a straight, 3 of a kind, 2 pair and pair. If we can't find these hands")
-    f.write("  then the only option left is a high card.\n")
+    f.write("  \n\n\n\n\n  /*\n")
+    f.write("    We now check for a straight, 3 of a kind, 2 pair and pair. If we can't find these hands\n")
+    f.write("    then the only option left is a high card.\n")
     f.write("  */\n\n")
     
     # Initial switch declaration
@@ -243,11 +273,11 @@ with open(outFile, "a") as f:
     # High card
     #
     
-    f.write("  \n\n\n\n\n/*\n")
-    f.write("  If we have made it this far the only option left is a high card.\n")
+    f.write("  \n\n\n\n\n  /*\n")
+    f.write("    If we have made it this far the only option left is a high card.\n")
     f.write("  */\n\n")
     
     f.write("  return 1;\n\n")
 
     # End of the function
-    f.write("}\n")
+    f.write("}\n\n\n\n")
